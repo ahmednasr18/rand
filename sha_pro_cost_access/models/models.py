8b84e8232f18c18a9ef4ad60af382671177a9733
TechScopeCo <hatem.hassan@TechScopeco.com>
2024-10-08 13:36:05 +0300

    update

--- sha_pro_cost_access/models/models.py
+++ sha_pro_cost_access/models/models.py
@@ -3,28 +3,43 @@
 from odoo import models, fields, api
 
 
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    cost_readonly = fields.Boolean(compute='_compute_cost_readonly')
+
+    def _compute_cost_readonly(self):
+        user = self.env.user
+        for rec in self:
+            if user.has_group("sha_pro_cost_access.group_access_product_cost"):
+                rec.cost_readonly = True
+            else:
+                rec.cost_readonly = False
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    standard_price = fields.Float(
-        'Cost', compute='_compute_standard_price',
-        inverse='_set_standard_price', search='_search_standard_price',
-        digits='Product Price',
-        # groups="",
-        groups="!sha_pro_cost_access.group_access_product_cost",
-        help="""Value of the product (automatically computed in AVCO).
-            Used to value the product when the purchase cost is not known (e.g. inventory adjustment).
-            Used to compute margins on sale orders.""")
+    cost_readonly = fields.Boolean(compute='_compute_cost_readonly')
+
+    def _compute_cost_readonly(self):
+        user = self.env.user
+        for rec in self:
+            if user.has_group("sha_pro_cost_access.group_access_product_cost"):
+                rec.cost_readonly = True
+            else:
+                rec.cost_readonly = False
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    standard_price = fields.Float(
-        'Cost', company_dependent=True,
-        digits='Product Price',
-        # groups="",
-        groups="!sha_pro_cost_access.group_access_product_cost",
-        help="""Value of the product (automatically computed in AVCO).
-            Used to value the product when the purchase cost is not known (e.g. inventory adjustment).
-            Used to compute margins on sale orders.""")
+    cost_readonly = fields.Boolean(compute='_compute_cost_readonly')
+
+    def _compute_cost_readonly(self):
+        user = self.env.user
+        for rec in self:
+            if user.has_group("sha_pro_cost_access.group_access_product_cost"):
+                rec.cost_readonly = True
+            else:
+                rec.cost_readonly = False
