f00c2aa17987886819a9eb2a8fb20379bf694c25
TechScopeCo <hatem.hassan@TechScopeco.com>
2024-10-21 09:29:04 +0300

    cost_access

---
+++ sha_pro_cost_access/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Product Cost Access",
+    "summary": "This module contains adding access to product cost.",
+    "description": """
+        This module contains adding access to product cost.
+    """,
+    "author": "TechScope",
+    "website": "www.techscopeco.com",
+    "depends": ["base", "sale_management", "sale"],
+    "data": [
+        # 'security/ir.model.access.csv',
+        "security/groups.xml",
+        "views/views.xml",
+    ],
+}
---
+++ sha_pro_cost_access/models/models.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    # cost_readonly = fields.Boolean(compute="_compute_cost_readonly")
+
+    # @api.depends("order_id")
+    # @api.depends_context("uid")
+    # def _compute_cost_readonly(self):
+    #     user = self.env.user
+    #     for rec in self:
+    #         if user.has_group("sha_pro_cost_access.group_access_product_cost"):
+    #             rec.cost_readonly = True
+    #         else:
+    #             rec.cost_readonly = False
+
+    cost_readonly = fields.Boolean(
+        compute="_check_user_group",
+        default=lambda self: self.env.user.has_group(
+            "sha_pro_cost_access.group_access_product_cost"
+        ),
+    )
+
+    def _check_user_group(self):
+        self.cost_readonly = self.env.user.has_group(
+            "sha_pro_cost_access.group_access_product_cost"
+        )
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    # cost_readonly = fields.Boolean(compute='_compute_cost_readonly')
+
+    # @api.depends('list_price')
+    # @api.depends_context('uid')
+    # def _compute_cost_readonly(self):
+    #     user = self.env.user
+    #     for rec in self:
+    #         if user.has_group("sha_pro_cost_access.group_access_product_cost"):
+    #             rec.cost_readonly = True
+    #         else:
+    #             rec.cost_readonly = False
+    cost_readonly = fields.Boolean(
+        compute="_check_user_group",
+        default=lambda self: self.env.user.has_group(
+            "sha_pro_cost_access.group_access_product_cost"
+        ),
+    )
+
+    def _check_user_group(self):
+        self.cost_readonly = self.env.user.has_group(
+            "sha_pro_cost_access.group_access_product_cost"
+        )
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    # cost_readonly = fields.Boolean(compute="_compute_cost_readonly")
+
+    # @api.depends("list_price")
+    # @api.depends_context("uid")
+    # def _compute_cost_readonly(self):
+    #     user = self.env.user
+    #     for rec in self:
+    #         if user.has_group("sha_pro_cost_access.group_access_product_cost"):
+    #             rec.cost_readonly = True
+    #         else:
+    #             rec.cost_readonly = False
+
+    cost_readonly = fields.Boolean(
+        compute="_check_user_group",
+        default=lambda self: self.env.user.has_group(
+            "sha_pro_cost_access.group_access_product_cost"
+        ),
+    )
+
+    def _check_user_group(self):
+        self.cost_readonly = self.env.user.has_group(
+            "sha_pro_cost_access.group_access_product_cost"
+        )
